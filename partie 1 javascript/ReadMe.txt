52eaac91007baac815305ccd1123cc2409fd1fc3
clemonboard <clement.bricout@orange.fr>
2018-04-20 10:54:00 -0400

    lab responsive

---
+++ partie 1 javascript/ReadMe.txt
@@ -0,0 +1,5 @@
+salut salut,
+
+il y a un dossier "police", si tu veut voir mon site bien presenter tu dois les instaler sur ton ordinateur.
+
+bonne visite :)
---
+++ partie 1 javascript/scriptresponsive.js
@@ -0,0 +1,9 @@
+
+    if (screen.width > 1366)
+	{
+              document.write('<link rel="stylesheet" href="style.css" type="text/css"  media="screen,projection" />')
+	}
+        else
+		{
+            document.write('<link rel="stylesheet" href="css/style2.css" type="text/css"  media="screen,projection" />')
+		}
